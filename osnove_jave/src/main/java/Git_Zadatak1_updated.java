a177ae0891508e821a05e9dea653163ad07ef5c0
Miljana Cvetkovic <miljana.cv@hotmail.com>
2022-05-13 21:47:42 +0200

    Delete class

--- osnove_jave/src/main/java/Git_Zadatak1_updated.java
+++
@@ -1,8 +0,0 @@
-public class Git_Zadatak1_updated {
-    public static void main(String[] args) {
-        System.out.println("git clone");
-        System.out.println("git add");
-        System.out.println("git commit");
-        System.out.println("git push");
-    }
-}
